5ae10fc8e93cf73bc647cc5a316f813deb2facef
johndeyrup <john.deyrup@gmail.com>
2016-06-05 14:40:42 -0400

    Added some stuff

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,7 +8,12 @@
  * Controller of the housingProSeApp
  */
 angular.module('housingProSeApp')
-  .controller('MainCtrl', function ($scope, cfg) {
+  .controller('MainCtrl', function ($scope, API_URL, $http) {
+  	$scope.tests = API_URL;
+  	var data = {"_source": false, "size": 0, "aggs": {"2": {"terms": "ExecutionIssued", "size": 5}}}
+  	$http.post(API_URL, data)
+  	.then(function(response) {
+  		$scope.test = response;
 
-  	$scope.test = "config.API_URL";
+  	});
   });
